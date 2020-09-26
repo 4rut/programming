034e153b36aa776399dabae1a39e989594c6e611
4rut <armenmen28@gmail.com>
2020-09-26 15:03:53 +0300

    third commit

---
+++ Practice/06/C++/Project1/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	double a, b, c;
+	std::cin >> a >> b >> c;
+
+	if (a == 0)
+	{
+		std::cout << "x = " << -c / b << '\n';
+	}
+	else
+	{
+		double D = b * b - 4 * a * c;
+		if (D < 0) {
+			std::cout << "Корней нет\n";
+		}
+		else if (D == 0)
+		{
+			std::cout << "x = " << -b / (2 * a) << '\n';
+		}
+		else
+		{
+			std::cout << "x1 = " << (-b + sqrt(D)) / (2 * a) << '\n';
+			std::cout << "x2 = " << (-b - sqrt(D)) / (2 * a) << '\n';
+		}
+
+	}
+
+	system("pause");
+	return 0;
+}
