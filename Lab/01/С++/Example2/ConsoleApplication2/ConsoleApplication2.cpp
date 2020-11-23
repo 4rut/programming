6c51b9106af9734d11b087a0e2010496c2418592
4rut <armenmen28@gmail.com>
2020-11-23 18:10:26 +0300

    lab1

---
+++ Lab/01/С++/Example2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cpp_httplib/httplib.h>
+
+using namespace httplib;
+
+int main() 
+{
+	Client cli("http://worldtimeapi.org");
+	auto res = cli.Get("/api/timezone/Europe/Simferopol");
+	if (res) {
+		if (res->status == 200) {
+			std::cout << res->body << std::endl;
+		} else {
+			std::cout << "Status code: " << res->status << std::endl;
+		}
+	}
+	else 
+	{
+		auto err = res.error();
+		std::cout << "Error code: " << err << std::endl;
+	}
+}
