8712d53594066e1978475f69b10246b6f6f749ba
4rut <armenmen28@gmail.com>
2020-12-01 11:21:34 +0300

    01.12.2020 commit

---
+++ Practice/25/C++/bozoSort/bozoSort/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+std::vector<int> bozoSort(std::vector<int> arr, bool bl=1) 
+{
+
+}
+
+std::vector <std::vector<int>> bozoSort(std::vector<std::vector<int>> arr, bool bl = 1)
+{
+
+}
+
+int bozoSort(int a, int b, int c, bool bl = 1)
+{
+
+}
+
+int main() 
+{
+
+	return 0;
+}
